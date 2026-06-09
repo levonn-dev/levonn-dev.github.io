d439da9c0498c1e22ff96fdb2d455aa87e8173f9
levonn-dev <levonn@levonn.org>
2026-06-09 13:10:34 -0400

    fix: rename ls to dir

--- pages/terminal/commands/registry.js
+++ pages/terminal/commands/registry.js
@@ -1,6 +1,6 @@
 export { default as help }  from './help.js';
 export { default as about } from './about.js';
-export { default as ls }    from './ls.js';
+export { default as dir }   from './dir.js';
 export { default as open }  from './open.js';
 export { default as clear } from './clear.js';
 export { default as bsod }  from './bsod.js';
